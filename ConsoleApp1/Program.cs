1be439a7b08984f166f451fed6c0fa90771bf6ae
Franko Lovrinović <flovri01@fesb.hr>
2025-04-06 19:05:47 +0200

    nez

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,9 +5,11 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-            {
-                Console.WriteLine("Hello, World!");
-            }
+        {
+               
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Novo");
+        }
     }
 }
 
